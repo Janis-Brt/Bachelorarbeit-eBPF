2e09ac4a9d1c0dfb944701eabd80ef81b7d0f6a1
= <janis.burtsche@studmail.htw-aalen.de>
2023-03-31 10:26:25 +0200

    Initial commit

---
+++ eBPF.py
@@ -0,0 +1,47 @@
+import signal
+import sys
+
+from bcc import BPF
+
+prog = """ 
+struct data_t {
+    int syscallnumber;
+};
+
+int sgettimeofday(struct pt_regs *ctx) {
+    struct data_t data = {};
+    data.syscallnumber = 0;
+    events.perf_submit(ctx, &data, sizeof(data));
+    return 0;
+}
+"""
+b = BPF(text=prog)
+
+
+def attachkretprobe():
+    b.attach_kretprobe(event=b.get_syscall_fnname("gettimeofday"), fn_name="sgettimeofday")
+
+
+def updateoccurences():
+    print("found gettimeofdate!")
+
+
+def getringbuffer():
+    uptime = 0
+    b["events"].open_perf_buffer(updateoccurences, page_cnt=256)
+    while True:
+        try:
+            b.perf_buffer_poll(timeout=10 * 1000)
+        except KeyboardInterrupt:
+            print("Abbruch")
+            signal_handler(signal.SIGINT, signal_handler)
+
+
+def signal_handler(sig, frame):
+    print('Exited with Keyboard Interrupt')
+    sys.exit(0)
+
+
+attachkretprobe()
+print("attachment ready" + "\n" + "now tracing! \npress CTRL + C to stop tracing.")
+getringbuffer()
