afd153e0766256541938cda30cd2238906b1f362
= <janis.burtsche@studmail.htw-aalen.de>
2023-04-14 10:34:06 +0200

    fünfter Versuch inum

--- eBPF.py
+++ eBPF.py
@@ -20,7 +20,7 @@ int sgettimeofday(struct pt_regs *ctx) {
     u32 pid = bpf_get_current_pid_tgid();
     pid = pid >> 32;
     struct task_struct *t = (struct task_struct *)bpf_get_current_task();
-    u32 inum= t->nsproxy->pid_namespaces->ns_common->inum;
+    u32 inum= t->nsproxy->pid_namespace->ns_common->inum;
     bpf_trace_printk("pid=%d; upid=%d!\\n", pid, inum);
     return 0;
 
