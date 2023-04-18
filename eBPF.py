42305ea1d2f86901a21f556a03a9f44ed6e0b2e2
= <janis.burtsche@studmail.htw-aalen.de>
2023-04-18 18:13:10 +0200

    Inserted some syscalls for testing + debug 3

--- eBPF.py
+++ eBPF.py
@@ -517,8 +517,9 @@ def detectpatterns(cpu, data, size):
     cgroup = data.cgroup
     # if localpids.__contains__(str(pid)):
     if syscall == 0:
-        if inum_ring != 4026531836:
-            print(syscall)
+        if inum_ring == 4026531836:
+            print("Outside Container")
+        else:
             if syscall == 0:
                 print("found clone inside the Container! with inum: " + str(inum_ring))
                 syscall = "clone"
