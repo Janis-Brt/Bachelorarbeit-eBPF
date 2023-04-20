99106784cd8a22bfe87491cdd537f73997b44020
= <janis.burtsche@studmail.htw-aalen.de>
2023-04-20 19:27:55 +0200

    PID Namespace test 4

--- eBPF.py
+++ eBPF.py
@@ -2765,7 +2765,7 @@ def getinum():
     for part in parts:
         if part.__contains__("pid:["): # and part.endswith("]'\n"):
             # Extrahiere die ID aus der Zeichenkette
-            pid_ns_id = part[5:-4]
+            pid_ns_id = part[5:-3].strip()
             break
 
     if pid_ns_id:
