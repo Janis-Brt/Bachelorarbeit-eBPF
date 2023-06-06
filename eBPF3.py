6277c569bc4f838b46b51b96030b5f7b340df3de
= <janis.burtsche@studmail.htw-aalen.de>
2023-06-06 15:14:01 +0200

    INUM replacement test 15

--- eBPF3.py
+++ eBPF3.py
@@ -82,7 +82,6 @@ int sread(struct pt_regs *ctx) {
     u32 tgid = bpf_get_current_pid_tgid();
     data.tgid = tgid;
     data.syscallnumber = 2;
-    data.test_inum = inum_container;
     events.perf_submit(ctx, &data, sizeof(data));
     return 0;
 }
