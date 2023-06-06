7a92c6aadf0dcc111f3fc6528e09161d78d51d2c
= <janis.burtsche@studmail.htw-aalen.de>
2023-06-06 16:05:53 +0200

    ARRAY Test 3

--- eBPF3.py
+++ eBPF3.py
@@ -23,7 +23,7 @@ struct data_t {
 // Initialisierung des BPF Ring Buffers. Mit diesem kann man Daten an den Userspace übergeben
 BPF_PERF_OUTPUT(events);
 BPF_ARRAY(inums, unsigned int, 128);
-inums.add(1234);
+inums.increment(1234);
 
 // INUM_RING
 // bpf_trace_printk("Wert der Variable: %u", inum_container);
