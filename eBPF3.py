56beea44f8d2c774e39be6f00624cfe39fd4148b
= <janis.burtsche@studmail.htw-aalen.de>
2023-06-07 21:13:28 +0200

    Reset, wo es noch geklappt hat :D

--- eBPF3.py
+++ eBPF3.py
@@ -111,6 +111,7 @@ int sread(struct pt_regs *ctx) {
     INUM_RING
     struct task_struct *t = (struct task_struct *)bpf_get_current_task();
     unsigned int inum_ring = t->nsproxy->pid_ns_for_children->ns.inum;
+    int i = 0;
     if(PT_REGS_RC(ctx) < 0 || inum_container != inum_ring){
         return 0;
     }
