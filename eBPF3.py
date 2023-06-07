014614aff31e7398c01cad9d2c82ff91c13dd5d5
= <janis.burtsche@studmail.htw-aalen.de>
2023-06-07 20:39:14 +0200

    ret_value ist nun das Ergebnis des lookups return value um zu schauen, ob das der inode entspricht. Dann habe ich es erneuter test

--- eBPF3.py
+++ eBPF3.py
@@ -48,7 +48,7 @@ int inums_update(unsigned int inum) {
     return 0;
 }
 
-static int inums_lookup(unsigned int inum){
+static unsigned int inums_lookup(unsigned int inum){
     unsigned int *value = inums.lookup(&inum);
     if (value) {
         // Die inum existiert im Array inums
