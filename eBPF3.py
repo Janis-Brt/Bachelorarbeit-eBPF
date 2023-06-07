88170790dd4ac2866bfcb40cff03a15654cdb9ce
= <janis.burtsche@studmail.htw-aalen.de>
2023-06-07 21:06:40 +0200

    nur noch write jetzt erneut

--- eBPF3.py
+++ eBPF3.py
@@ -126,13 +126,12 @@ int sread(struct pt_regs *ctx) {
 
 int swrite(struct pt_regs *ctx) {
     // hier auf return Value zugreifen
-    int inum_init();
     struct data_t data = {};
     INUM_RING
-    int ret_value = inums_lookup(inum_container);
-    data.test_inum = ret_value;
     struct task_struct *t = (struct task_struct *)bpf_get_current_task();
     unsigned int inum_ring = t->nsproxy->pid_ns_for_children->ns.inum;
+    int ret_value = inums_lookup(inum_container);
+    data.test_inum = ret_value;
     if(PT_REGS_RC(ctx) < 0 || ret_value != 0){
         return 0;
     }
