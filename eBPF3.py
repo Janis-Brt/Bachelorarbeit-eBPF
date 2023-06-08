aa295d0fc13104a3cb48e177e11fb94b92cbf56f
= <janis.burtsche@studmail.htw-aalen.de>
2023-06-08 11:27:00 +0200

    try and Error

--- eBPF3.py
+++ eBPF3.py
@@ -28,7 +28,7 @@ BPF_PERF_OUTPUT(events);
 
 BPF_ARRAY(inums, u64, 128);
 
-static u64 inums_init(struct pt_regs *ctx) {
+static u64 inums_init() {
     INUM_RING
     inums.atomic_increment(inum_container);
     return inum_container;
