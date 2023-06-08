ee34fcc7799d6737e1ae156b9db2f86b2709e711
= <janis.burtsche@studmail.htw-aalen.de>
2023-06-08 11:10:50 +0200

    mit return value erneut test

--- eBPF3.py
+++ eBPF3.py
@@ -45,7 +45,7 @@ int inums_update(unsigned int inum) {
 
 static int inums_lookup(unsigned int inum){
     int inum_init();
-    unsigned int *value = inums.lookup(&inum);
+    u64 *value = inums.lookup(&inum);
     if (value==NULL) {
         return 1;  // Wert inum im Array gefunden
     }
