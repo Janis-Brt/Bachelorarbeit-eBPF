1647960d168c854e79371efe4460268f7d52f584
= <janis.burtsche@studmail.htw-aalen.de>
2023-06-08 11:49:38 +0200

    rebase

--- eBPF3.py
+++ eBPF3.py
@@ -41,6 +41,7 @@ int inums_update(unsigned int inum) {
 
 static int inums_lookup(unsigned int inum){
     int inum_init();
+    int i = 0;
     unsigned int *value = inums.lookup(&inum);
     if (value==NULL) {
         return 1;  // Wert inum im Array gefunden
