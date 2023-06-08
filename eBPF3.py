d9bd4703a5a0f62128e0d2ff50ccf7458d1327d1
= <janis.burtsche@studmail.htw-aalen.de>
2023-06-08 09:58:11 +0200

    with ret value of inums_init

--- eBPF3.py
+++ eBPF3.py
@@ -99,6 +99,7 @@ int sread(struct pt_regs *ctx) {
     INUM_RING
     struct task_struct *t = (struct task_struct *)bpf_get_current_task();
     unsigned int inum_ring = t->nsproxy->pid_ns_for_children->ns.inum;
+    int ret_init = inums_init();
     int ret_value = inums_lookup(inum_container);
     data.test_inum = ret_value;
     if(PT_REGS_RC(ctx) < 0 || inum_container != inum_ring){
