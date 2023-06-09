0999ca9016ca2635932ae717cfc1227a42f25d42
= <janis.burtsche@studmail.htw-aalen.de>
2023-06-09 09:41:10 +0200

    mit ret value ausgabe verbessert und mit if

--- eBPF3.py
+++ eBPF3.py
@@ -110,7 +110,9 @@ int sread(struct pt_regs *ctx) {
     // data.test_inum = ret_value;
     // data.init_return = ret_init;
     int ret_value = inums_lookup(inum_ring);
-    bpf_trace_printk("Ret Value read: %d" , ret_value);
+    if(ret_value == 0){
+        bpf_trace_printk("Ret Value read: %d" , ret_value);
+    }
     if(PT_REGS_RC(ctx) < 0 || ret_value != 0){
         return 0;
     }
