7ad3a4eaaacf24f9eb0f6377eb46a2be5b24efb7
= <janis.burtsche@studmail.htw-aalen.de>
2023-06-09 07:58:04 +0200

    Debug Messages 4.5

--- eBPF3.py
+++ eBPF3.py
@@ -103,7 +103,9 @@ int sread(struct pt_regs *ctx) {
     unsigned int inum_ring = t->nsproxy->pid_ns_for_children->ns.inum;
     u64 ret_init = inums_init();
     int ret_value = inums_lookup(inum_ring);
-    bpf_trace_printk("Lookup Return-Value %d\\n", ret_value);
+    if(ret_value == 0){
+        bpf_trace_printk("Lookup Return-Value ist 0 %d\\n", ret_value);
+    }
     data.test_inum = ret_value;
     data.init_return = ret_init;
     if(PT_REGS_RC(ctx) < 0 || inum_container != inum_ring){
