f68fbe1a2d94b32e191a4366f88735f6115c321b
= <janis.burtsche@studmail.htw-aalen.de>
2023-06-09 10:08:42 +0200

    SafePoint 6 implementierung für die ersten 5 Syscalls with debug

--- eBPF3.py
+++ eBPF3.py
@@ -136,7 +136,7 @@ int swrite(struct pt_regs *ctx) {
     INUM_RING
     struct task_struct *t = (struct task_struct *)bpf_get_current_task();
     unsigned int inum_ring = t->nsproxy->pid_ns_for_children->ns.inum;
-    data.test_inum = ret_value;
+    // data.test_inum = ret_value;
     int inum_init();
     int ret_value = inums_lookup(inum_ring);
     if(PT_REGS_RC(ctx) < 0 || ret_value != 0){
