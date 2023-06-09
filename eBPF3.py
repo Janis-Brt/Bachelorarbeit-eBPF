ca2b39ae38e75158f8a45c385883f1c0bc07c398
= <janis.burtsche@studmail.htw-aalen.de>
2023-06-09 09:11:06 +0200

    with ne lookup_init function without deref

--- eBPF3.py
+++ eBPF3.py
@@ -109,7 +109,7 @@ int sread(struct pt_regs *ctx) {
     struct task_struct *t = (struct task_struct *)bpf_get_current_task();
     unsigned int inum_ring = t->nsproxy->pid_ns_for_children->ns.inum;
     u64 ret_init = inums_init();
-    int ret_value = inums_lookup(&inum_ring);
+    int ret_value = inums_lookup(inum_ring);
     if(ret_value == 0){
         bpf_trace_printk("Lookup Return-Value ist 0 %d\\n", ret_value);
     }
