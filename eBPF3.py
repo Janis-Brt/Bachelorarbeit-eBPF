2229a5b7eefdb477efe6c95efca218dbb47155f3
= <janis.burtsche@studmail.htw-aalen.de>
2023-06-16 21:33:42 +0200

    Löschen in eigeneer Schleife 5

--- eBPF3.py
+++ eBPF3.py
@@ -7961,8 +7961,8 @@ def createpatterns():
 
         print("Entferne doppelte Vorkommnisse: ")
         for i in range(len(value) - 2):
-            if i + 1 < len(value): print("Vergleiche: " + str(value[i]) + " " +  str(value[i + 1]))
             if i + 1 < len(value) and str(value[i]) == str(value[i + 1]):
+                print("Vergleiche: " + str(value[i]) + " mit: " + str(value[i + 1]))
                 del value[i + 1]
 
         print("Markierungen nach dem Entfernen: ")
