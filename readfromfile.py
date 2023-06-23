7ef88f5ceea0f723f3bc3e0e7d7114139d7bfddd
= <janis.burtsche@studmail.htw-aalen.de>
2023-06-23 10:19:39 +0200

    Test mit Auslese Script 2

--- readfromfile.py
+++ readfromfile.py
@@ -4,15 +4,21 @@ import glob
 
 # Pfad zum Verzeichnis, in dem die Dateien gesucht werden sollen
 path = "data/patternspid/"
+path2 = "data/patternstgid"
 # Alle .txt-Dateien im Verzeichnis auflisten
 txt_files = glob.glob(path + "*.txt")
 
 if txt_files:
     # Erste gefundene .txt-Datei öffnen und lesen
-    with open(txt_files[0], "r") as file:
+    with open(path, "r") as file:
         lines = file.readlines()
+    with open(path2, "r") as file:
+        lines2 = file.readlines()
 
     for line in lines:
+        print(line)
+    print("++++++++++++++++++++++++++++++++++++++++++++++")
+    for line in lines2:
         print(line)
 else:
     print("Keine .txt-Dateien gefunden.")
