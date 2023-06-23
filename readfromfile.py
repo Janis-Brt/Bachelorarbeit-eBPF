671d28e98ffea89a790e33a99cd415cdeb08fe4d
= <janis.burtsche@studmail.htw-aalen.de>
2023-06-23 10:21:20 +0200

    Test mit Auslese Script 4

--- readfromfile.py
+++ readfromfile.py
@@ -7,12 +7,13 @@ path = "data/patternspid/"
 path2 = "data/patternstgid/"
 # Alle .txt-Dateien im Verzeichnis auflisten
 txt_files = glob.glob(path + "*.txt")
+txt_files2 = glob.glob(path2 + "*.txt")
 
 if txt_files:
     # Erste gefundene .txt-Datei öffnen und lesen
-    with open(path + "*.txt", "r") as file:
+    with open(txt_files[0], "r") as file:
         lines = file.readlines()
-    with open(path2 + "*.txt", "r") as file:
+    with open(txt_files2[0], "r") as file:
         lines2 = file.readlines()
 
     for line in lines:
