13e0ba267e486c43a2696ba7034af3c1e8ed1104
janis <janis.burtsche@studmail.htw-aalen.de>
2023-06-27 08:46:29 +0200

    PID-NS new try

--- BA-Prototype.py
+++ BA-Prototype.py
@@ -57,13 +57,18 @@ int sclone(struct pt_regs *ctx) {
     struct data_t data = {};
     INUM_RING
 
-    // Erhalte die PID-Namespace Inum von clone()
+    // Erhalte die PID-Namespace Inum von clone() old try
+    // struct task_struct *t = (struct task_struct *)bpf_get_current_task();
+    // unsigned int inum_ring = t->nsproxy->pid_ns_for_children->ns.inum;
+    
+    
+    // Erhalte den PID-Namespace von clone() new try
     struct task_struct *t = (struct task_struct *)bpf_get_current_task();
-    unsigned int inum_ring = t->nsproxy->pid_ns_for_children->ns.inum;
+    struct task_struct *parent = t->real_parent;
+    unsigned int inum_ring_child = parent->nsproxy->pid_ns_for_children->ns.inum;
 
-    // Erhalte die PID-Namespace Inum des erzeugten Kind Prozesses von clone()
-    struct pid_namespace *pid_ns = t->nsproxy->pid_ns_for_children;
-    unsigned int inum_ring_child = pid_ns->ns.inum;
+    // Erhalte den PID-Namespace des erzeugten Kind Prozesses von clone()
+    unsigned int inum_ring = t->nsproxy->pid_ns_for_children->ns.inum;
 
     // Füge die Child Inum hinzu, falls sie abweicht von der Parent Inum
     if(inum_ring_child != inum_ring){
