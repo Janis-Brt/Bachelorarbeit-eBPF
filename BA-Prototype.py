3a3b8bcf460966f68b738a8d7901839fc4bd4490
janis <janis.burtsche@studmail.htw-aalen.de>
2023-06-27 08:36:29 +0200

    IndexError fix? 3

--- BA-Prototype.py
+++ BA-Prototype.py
@@ -7913,15 +7913,17 @@ def createpatternspid():
         #     print(value[i])
         # print("++++++++++++++++++++++++++++++++++++++++++++")
         # print("Entferne doppelte Vorkommnisse in PID List: ")
-        i = 0
-        while i < len(value):
-            if i + 1 < len(value) and str(value[i]) == str(value[i + 1]):
-                # print("Vergleiche: " + str(value[i]) + " mit: " + str(value[i + 1]))
-                # print("Lösche: " + str(value[i + 1]))
-                del value[i + 1]
-                i = i - 1
-            else:
-                i += 1
+        try:
+            i = 0
+            while i < len(value):
+                if i + 1 < len(value) and str(value[i]) == str(value[i + 1]):
+                    # print("Vergleiche: " + str(value[i]) + " mit: " + str(value[i + 1]))
+                    # print("Lösche: " + str(value[i + 1]))
+                    del value[i + 1]
+                    i = i - 1
+                else:
+                    i += 1
+        except: IndexError
         # print("++++++++++++++++++++++++++++++++++++++++++++")
         # Schleife von 0 bis Länge der Liste minus 2
         for i in range(len(value) - 2):
